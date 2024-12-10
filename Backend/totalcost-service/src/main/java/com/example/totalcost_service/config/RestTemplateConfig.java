867055604e5a336ba9481be1394ca41d65861375
sebsatian <sebastian.paillao@usach.cl>
2024-12-10 13:33:53 -0300

    improved structure and configurations

--- Backend/totalcost-service/src/main/java/com/example/totalcost_service/config/RestTemplateConfig.java
+++ Backend/totalcost-service/src/main/java/com/example/totalcost_service/config/RestTemplateConfig.java
@@ -1,6 +1,4 @@
 package com.example.totalcost_service.config;
-
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
